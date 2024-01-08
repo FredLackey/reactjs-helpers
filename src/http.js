3b19e003d9acf5958d8c1b703f4a5330df9a3072
Fred Lackey <fred.lackey@gmail.com>
2024-01-08 06:18:39 -0500

    v0.0.9

--- src/http.js
+++ src/http.js
@@ -49,20 +49,37 @@ const addHeaders = (creds = {}, headers = {}) => {
   return result;
 };
 
-const doPromise = ({
-  method = 'GET', url, data, creds = {}, headers = {},
-}) => {
-  return fetch(toUrl(url), {
-      // credentials: 'same-origin', // 'include', default: 'omit'
-    method,
-    body   : toBody(data),
-    headers: addHeaders(creds, headers),
-  })
-    .then(response => ({
-      status: response.status,
-      data  : response.json()
-    }))
-    .catch(error => console.debug(error));
+const doPromise = async ({ method = 'GET', url, data, creds = {}, headers = {} }) => {
+  
+  url = toUrl(url);
+
+  try {
+    const response = await fetch(url, {
+      method,
+      body   : toBody(data),
+      headers: addHeaders(creds, headers),
+    });
+    const json   = await response.json();
+    const status = response.status;
+    const ok     = status >= 200 && status < 300;
+    const body   = ok ? json : null;
+    const error  = ok ? null : json;
+    return {
+      ok,
+      status,
+      data: body,
+      error
+    };
+  } catch (ex) {
+    return {
+      ok    : false,
+      status: -1,
+      data  : null,
+      error : `Failure calling ${url}`,
+      debug : ex
+    };
+  }
+
 };
 
 export const doGet = async (url, creds = {}, headers = {}) => {
